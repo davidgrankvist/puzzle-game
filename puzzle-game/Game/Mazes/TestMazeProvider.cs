3e88963dc5a1aa6a50e57f696f2d2b45fe305161
David Grankvist <grankvist.david@gmail.com>
2024-04-08 21:31:44 +0200

    feat: add more blocks

--- puzzle-game/Game/Mazes/TestMazeProvider.cs
+++ puzzle-game/Game/Mazes/TestMazeProvider.cs
@@ -11,22 +11,21 @@ namespace puzzle_game.Game.Mazes
 	{
 		public Maze GetMaze()
 		{
-			var borderTop = new MazeBlock(0, -1, Constants.WINDOW_WIDTH, 1)
-			{
-				ShouldRender = false,
-			};
-			var borderLeft = new MazeBlock(-1, 0, 1, Constants.WINDOW_HEIGHT)
-			{
-				ShouldRender = false,
-			};
-			var borderRight = new MazeBlock(Constants.WINDOW_WIDTH, 0, 1, Constants.WINDOW_HEIGHT)
-			{
-				ShouldRender = false,
-			};
+			var borderTop = new MazeBlock(0, 0, Constants.WINDOW_WIDTH, 50);
+			var borderLeft = new MazeBlock(0, 0, 50, Constants.WINDOW_HEIGHT);
+			var borderRight = new MazeBlock(Constants.WINDOW_WIDTH - 50, 0, 50, Constants.WINDOW_HEIGHT);
 			var ground = new MazeBlock(0, Constants.WINDOW_HEIGHT - 50, Constants.WINDOW_WIDTH, 50);
-			var obstacle = new MazeBlock(400, Constants.WINDOW_HEIGHT - 100, 50, 50);
-			var flyingObstacle = new MazeBlock(450, Constants.WINDOW_HEIGHT - 200, 80, 50);
 
+			var topObstacle = new MazeBlock(300, 50, 50, 50);
+			var leftObstacle = new MazeBlock(50, 100, 100, 50);
+			var rightObstacle = new MazeBlock(Constants.WINDOW_WIDTH - 250, 250, 200, 50);
+			var groundObstacle1 = new MazeBlock(400, Constants.WINDOW_HEIGHT - 100, 50, 50);
+			var groundObstacle2 = new MazeBlock(600, Constants.WINDOW_HEIGHT - 100, 50, 50);
+
+			var flyingObstacle1 = new MazeBlock(450, Constants.WINDOW_HEIGHT - 200, 80, 50);
+			var flyingObstacle2 = new MazeBlock(500, Constants.WINDOW_HEIGHT - 300, 80, 50);
+			var flyingObstacle3 = new MazeBlock(600, Constants.WINDOW_HEIGHT - 400, 80, 50);
+			var flyingObstacle4 = new MazeBlock(200, Constants.WINDOW_HEIGHT - 400, 50, 100);
 
 			var maze = new Maze()
 			{
@@ -36,8 +35,15 @@ namespace puzzle_game.Game.Mazes
 					borderLeft,
 					borderRight,
 					ground,
-					obstacle,
-					flyingObstacle,
+					topObstacle,
+					leftObstacle,
+					rightObstacle,
+					groundObstacle1,
+					groundObstacle2,
+					flyingObstacle1,
+					flyingObstacle2,
+					flyingObstacle3,
+					flyingObstacle4,
 				}
 			};
 			return maze;
--- puzzle-game/game/common/Constants.cs
+++ puzzle-game/game/common/Constants.cs
@@ -9,7 +9,7 @@
         public const float PLAYER_SPEED = 5;
         public const float PLAYER_JUMP_SPEED = PLAYER_SPEED * 3;
         public const float PLAYER_GRAVITY = 1;
-		public const float PLAYER_START_X = 50f;
+		public const float PLAYER_START_X = 80f;
         public const float PLAYER_START_Y = WINDOW_HEIGHT / 2;
 
 		public const float CAMERA_ROTATION_SPEED = 0.5f;
